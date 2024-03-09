1369eee21fd7937fdeca520d41fd80b23da6722f
Syed Umair <umair_developer@outlook.com>
2024-03-09 16:23:41 -0500

    fontend: Fixed Indents in Sample Code

--- frontend/src/components/editor/langauge_metadata.tsx
+++ frontend/src/components/editor/langauge_metadata.tsx
@@ -20,19 +20,19 @@ export let sampleCodeMap = new Map<Language, string>([
   [LanguageName.JavaScript, `console.log("Hello, World!")`],
   [
     LanguageName.Golang,
-    `package main\n\nimport "fmt"\n\nfunc main() {\n    fmt.Println("Hello, World!")\n}`,
+    `package main\n\nimport "fmt"\n\nfunc main() {\n  fmt.Println("Hello, World!")\n}`,
   ],
-  [LanguageName.Rust, `fn main() {\n    println!("Hello, World!");\n}`],
+  [LanguageName.Rust, `fn main() {\n  println!("Hello, World!");\n}`],
   [
     LanguageName.CPlusPlus,
-    `#include <iostream>\n\nint main() {\n    std::cout << "Hello, World!" << std::endl;\n    return 0;\n}`,
+    `#include <iostream>\n\nint main() {\n  std::cout << "Hello, World!" << std::endl;\n  return 0;\n}`,
   ],
   [
     LanguageName.Java,
-    `public class HelloWorld {\n    public static void main(String[] args) {\n        System.out.println("Hello, World!");\n    }\n}`,
+    `public class HelloWorld {\n  public static void main(String[] args) {\n    System.out.println("Hello, World!");\n    }\n}`,
   ],
   [
     LanguageName.C,
-    `#include <stdio.h>\n\nint main() {\n    printf("Hello, World!\\n");\n    return 0;\n}`,
+    `#include <stdio.h>\n\nint main() {\n  printf("Hello, World!\\n");\n  return 0;\n}`,
   ],
 ]);
